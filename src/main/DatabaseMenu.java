e301d8412f12bd20714d4295203ed1d8029a451d
Michaellzd <735955506@qq.com>
2023-04-03 16:40:31 -0400

    set up your pwd

--- src/main/DatabaseMenu.java
+++ src/main/DatabaseMenu.java
@@ -9,9 +9,9 @@ import java.util.Scanner;
 
 
 public class DatabaseMenu {
-    private static final String DB_URL = "jdbc:mariadb://classdb2.csc.ncsu.edu:3306/zlu28";
-    private static final String DB_USER = "zlu28";
-    private static final String DB_PASSWORD = "200476848";
+    private static final String DB_URL = "";
+    private static final String DB_USER = "";
+    private static final String DB_PASSWORD = "";
 
     //setting your own pwd
 
